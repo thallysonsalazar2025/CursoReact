261bd855f3b0801e9f04d3296cc362ee8ed0c7da
Thallyson pontes <242952126+thallysonsalazar2025@users.noreply.github.com>
2026-04-30 17:05:03 -0300

    [REACT-001] TemplateExpressions - Adiciona um templateExpression

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
 import React from 'react';
 import './App.css';
 import FirstComponents from './components/FirstComponents';
-
+import TemplateExpressions from "./components/TemplateExpressions";
+// comentario comun
 function App() {
   return (
     <div className="App">
      <h1>Welcome to React App!</h1>
+        {/* Primeiro Desafio Incluir Tag na pagina */}
         <h1>Primeiro Desafio Incluir Tag na pagina</h1>
+        <p className="teste">Meu texto</p>
         <FirstComponents />
+        <TemplateExpressions />
     </div>
   );
 }
---
+++ src/components/TemplateExpressions.js
@@ -0,0 +1,10 @@
+const templateExpressions = () => {
+    const name = 'John Doe';
+    return (
+        <div>
+            <h1>Template Expressions</h1>
+            <p>Ola {name}, tudo bem?</p>
+        </div>
+    );
+};
+export default templateExpressions;
