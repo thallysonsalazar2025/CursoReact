3fe9ac3eb0fb95e90d0b7bc71b30c4954d444432
Thallyson pontes <242952126+thallysonsalazar2025@users.noreply.github.com>
2026-04-30 11:43:55 -0300

    [REACT-001] Cria o primeiro component e importa

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import FirstComponents from './components/FirstComponents';
 
 function App() {
   return (
     <div className="App">
      <h1>Welcome to React App!</h1>
+        <h1>Primeiro Desafio Incluir Tag na pagina</h1>
+        <FirstComponents />
     </div>
   );
 }
---
+++ src/components/FirstComponents.js
@@ -0,0 +1,10 @@
+const FirstComponents = () => {
+
+    return (
+        <div>
+            <h1>Primeiro Componente</h1>
+        </div>
+    );
+};
+
+export default FirstComponents;
