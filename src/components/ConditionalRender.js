eb17cdc18f7e9fb6fa94344da10887e9386a4c5f
Thallyson pontes <242952126+thallysonsalazar2025@users.noreply.github.com>
2026-05-02 16:22:03 -0300

    [REACT-002] FUNDAMENTOS- Exibição por condicional ternaria.

--- src/components/ConditionalRender.js
+++ src/components/ConditionalRender.js
@@ -2,10 +2,18 @@ import {useState} from "react";
 
 const ConditionalRender = () => {
     const [x] = useState(false);
+    const [name, setName] = useState("matheus");
     return (
         <div>
             <h1>Isso será exibido?</h1>
             {x && <p>Sim, pois x é verdadeiro</p>}
+            {/*condicao ternaria*/}
+            {name === "Joao" ?(
+                    <div><p>Ola Joao</p></div>
+            ): (
+                <div> <p>nome não encontrado</p> </div>
+                )}
+            <button onClick={() => setName("Joao")}>Clique aqui</button>
         </div>
     );
 };
